0414377ca4196c0f400eb03b9b28ba3b6e1526c9
Volodymyr Petrytsya <vpetrytsia@gmail.com>
2022-03-29 12:46:21 -0400

    saved messages page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
+import SavedMessages from "./containers/Home/SavedMessages";
+
 import NoMatch from "./containers/NoMatch/NoMatch";
 
 function App() {
@@ -19,6 +21,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/savedM" component={SavedMessages} />
         <Route  component={NoMatch} />
       </Switch>
     </Router>
